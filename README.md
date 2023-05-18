39009c9b50613e7a76ff58cfb766edfd063e9bbe
guymorganb <guymorganb@gmail.com>
2023-05-18 11:28:54 -0500

    final commit, added readme

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
 # Weather_App
+
+## Description
+
+- This document was made in order to serve as a learning opportunity for me to understand how to retrieve data from a 3rd party API, sort and use that data to be used to display information in a useful way.
+
+- This page was created to serve as a generic and simple app, used to view the weather forecast. As well as the current weather conditions
+
+- This project solves the problem of needing to demonstrate the ability to use JavaScript to dynamically update and manage the page dynamically use 3rd party API data and presenting that information usefully.
+
+- I was able to expand my knowleged of JavaScript after having been coding for only 5 months, Ive been learning how it can be applied to the working needs of a user. I was also able to understand the level of detail that goes into even something this simple.
+
+![step 1](https://github.com/guymorganb/Workday_App/blob/main/Assets/Screenshot.png?raw=true)
+
+## Table of Contents (N/A)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+This webpage can be navigated to and acessed with a browser or mobile device. It does not require installation.
+
+## Usage
+
+- This page can be used for learning and demonstration purposes, and for checking weather conditions now, and expected future conditions
+
+- To engage this document: Refer to image and see instructions below. Please note: data will be saved into your browsers local storage.
+
+1) First the user must click on the search box.
+
+2) The desired input is typed into the search field. Trailing whitespace, and numbers are not allowed as input data. Be careful to check that spelling is correct.
+
+3) Once the input is complete, click 'search' this will stored the input into local storage and create a list below the search box.
+
+4) To remove a single entry, click the red box next to the City which was searched. To remove all entries, click clear list to remove all entries at once.
+
+5) To view a past entry from the list, click the gray box (clicking the red box will populate the the dat but will also delete the single entry).
+
+6) Data is dynamically populated to show in the proper feilds, and dynamically removed.
+
+7) The current days weather is shown at the top of the page. the forecast for that location is shown below the current weather
+
+- Please follow this link to access the repository https://github.com/guymorganb/Weather_App
+
+
+- Please follow this link https://guymorganb.github.io/Weather_App/ to access the deployed page
+    
+
+## Credits
+
+EDx Bootcamp through the University of Denver provided the guidance and materials for the project.
+
+## License
+
+MIT License
+
+Copyright (c) [2023] [Guy MorganB]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Badges
+
+![badmath](https://img.shields.io/github/license/guymorganb/Weather_App)
+
+
+## Features
+
+This project does not have notable features outside of its usefulness as a learning opportunity and is function for checking the weather.
+
+## How to Contribute
+
+[Contributor Covenant](https://www.contributor-covenant.org/)
+
+## Tests
+
+Not tests avaliable at this time. I hope to learn more about how to make tests and implement them with my work.
--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,45 @@
-const apiKey = '4edd926bb76ff29ea1c821dc6c88a3ca'       // link to maybe an external document?
-//
-//let cityName =' denver ' // link to the input box and trim it let  // let 
-//let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
-
+/**
+ * JavaScript
+ * Weather App
+ *  
+ */
+// set global variables
+const apiKey = '4edd926bb76ff29ea1c821dc6c88a3ca'       // this is exposed.
 let forecast = $('#daysContainer');
 let searchHistory = $('hr');
-let timer = 'null'
-//let followMeOnInstagram = "https://www.instagram.com/guymorganb/"
-// /////////////////////////////////////////////////////
+let timer = 'null';
+// let followMeOnInstagram = "https://www.instagram.com/guymorganb/"
 
+// first function to query the API
 async function getData(city){
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     try{
-        let response = await fetch(queryURL)
+        let response = await fetch(queryURL);
         if(response.ok){
-            let weatherData = await response.json()
+            let weatherData = await response.json();
             return weatherData;
         }else{
-            throw new Error('Network respone not OK')
+            throw new Error('Network respone not OK');
         }
     }catch(error){
-        console.log('Error:')
-        error404()
-        console.log('Error:', error)
-        console.log('Error:after')
+        console.log('Error:');
+        error404();
+        console.log('Error:', error);
+        console.log('Error:after');
     }
 }
-function findCityData(city){ getData(city).then(function(weatherData){
-    clearCityData()
+// 2nd half of first api call using lattitue and longitude to target the dity
+function findCityData(city){getData(city).then(function(weatherData){
+    clearCityData();
     //console.log( weatherData)
-    let lat = weatherData.coord.lat
-    let long = weatherData.coord.lon
-    let queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`
+    let lat = weatherData.coord.lat;
+    let long = weatherData.coord.lon;
+    let queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`;
     async function getFutureCast(){
         try{
-            let response = await fetch(queryURL2)
+            let response = await fetch(queryURL2);
             if(response.ok){
-            let futureCast = await response.json()
+            let futureCast = await response.json();
             // set the current weather here
             let currCity = weatherData.name;
             // future: add timezone offset to display localtime
@@ -45,12 +48,12 @@ function findCityData(city){ getData(city).then(function(weatherData){
             // let timeZoneOffset = weatherData.sys.timezone
             // let localTime = dayjs(weatherData.coord.dt).format('hh:mm:ss')
             // console.log(localTime)
-            let currDate = dayjs(weatherData.coord.dt).format('M/D/YYYY') 
-            let currIcon = weatherData.weather[0].icon
-            let currAlt = weatherData.weather[0].description
-            let currTemp = parseFloat((weatherData.main.temp - 273.15)*(9/5)+32).toFixed(2)
-            let currWind = weatherData.wind.speed
-            let currhumidity = weatherData.main.humidity
+            let currDate = dayjs(weatherData.coord.dt).format('M/D/YYYY') ;
+            let currIcon = weatherData.weather[0].icon;
+            let currAlt = weatherData.weather[0].description;
+            let currTemp = parseFloat((weatherData.main.temp - 273.15)*(9/5)+32).toFixed(2);
+            let currWind = weatherData.wind.speed;
+            let currhumidity = weatherData.main.humidity;
             let currDisplay = $(`
             <div id="time">
              <h1 id="currentTown">${currCity}</h1>
@@ -66,33 +69,33 @@ function findCityData(city){ getData(city).then(function(weatherData){
                 <li id="humidity">Humidity: ${currhumidity}%</li>
             </ul>`)
             $('#townData').append(currDisplay);
-            return futureCast
+            return futureCast;
             }else{
                 throw new Error('Network response was not OK');
             }
         }
         catch(error){
-            console.log('Error', error)
-            error404()
+            console.log('Error', error);
+            error404();
         }
     }
-    // get the future cast
+    // get the future cast / (forecast)
     getFutureCast().then(function(futureCast){
-        console.log(futureCast)
-         // sorts the list inside the json object in asscending order by the dt_txt property
+        console.log(futureCast);
+         // sorts the list inside the json object in asscending order by the dt_txt property (not entirely needed but in case the data order changes)
          futureCast.list.sort(function(a, b) {     
             a.dt_txt - b.dt_txt;            
-           });
-           clearFutureCast()
-           //console.log(futureCast.list.length)
-           for(let i=2; i<futureCast.list.length; i+=7){
-            // loops through the futureCast of 5 days and append items from api
-            let date = dayjs(futureCast.list[i].dt_txt).format('M/D/YYYY')
-            let icon = futureCast.list[i].weather[0].icon
-            let alt = futureCast.list[i].weather[0].description
-            let temp = parseFloat(((futureCast.list[i].main.temp - 273.15)*(9/5)+32).toFixed(2))
-            let windSpeed = parseFloat((futureCast.list[i].wind.speed).toFixed(2))
-            let humidity = parseInt((futureCast.list[i].main.humidity))
+            });
+            clearFutureCast();
+            
+            for(let i=2; i<futureCast.list.length; i+=7){
+            // loops through the futureCast of 6 days and append items from api
+            let date = dayjs(futureCast.list[i].dt_txt).format('M/D/YYYY');
+            let icon = futureCast.list[i].weather[0].icon;
+            let alt = futureCast.list[i].weather[0].description;
+            let temp = parseFloat(((futureCast.list[i].main.temp - 273.15)*(9/5)+32).toFixed(2));
+            let windSpeed = parseFloat((futureCast.list[i].wind.speed).toFixed(2));
+            let humidity = parseInt((futureCast.list[i].main.humidity));
             let weather5DayDisplay = $(`
             <div class="days">
              <h3>${date}<h3>
@@ -103,148 +106,150 @@ function findCityData(city){ getData(city).then(function(weatherData){
                 <li id="humidity">Humidity: ${humidity}%</li>
             </ul>
             </div>`)
-            forecast.append(weather5DayDisplay)
-           }
-        })
-////The weather entries duplicate when a new city is entered, you gotta clear the dynamically created elements when a new city is entered
-})
-}
+            forecast.append(weather5DayDisplay);
+           };
+        });
+});
+};
+// function to show the error message
 function error404 (){
-    let notFound = $('<a id="errMsg"><img class="404" src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7888.jpg?w=2000" height="275px" alt="404 Not Found"></a>')
-    clearCityData()
-    clearFutureCast()
-    $('#townData').append(notFound)
+    let notFound = $('<a id="errMsg"><img class="404" src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7888.jpg?w=2000" height="275px" alt="404 Not Found"></a>');
+    clearCityData();
+    clearFutureCast();
+    $('#townData').append(notFound);
 }
-// adds to local storage and generates a key automatically for sorting later
+// adds inputs into local storage and generates a key automatically for sorting later
 function putIntoLocalStorage(elementToStore){
     if(localStorage.length <= 8){
-        let key = localStorage.length
-        localStorage.setItem(key, elementToStore)
-    }else(alert('To store other Cities please clear the list'))
+        let key = localStorage.length;
+        localStorage.setItem(key, elementToStore);
+    }else(alert('To store other Cities please clear the list'));
 }
 // validate input against existing localStorage element to avoid duplicates
 function checkInput(someInput){
     const isValid = /[a-zA-Z]/.test(someInput);
     if(!isValid){
-        alert('Invalid input: AlphaNumeric Chars only, with no whitespace.')
-        return
+        alert('Invalid input: AlphaNumeric Chars only, with no whitespace.');
+        return;
     }   
     else if(someInput == ''){
         alert('Invalid Input, spaces not allowed')
-        return
+        return;
     }    
     for(let i = 0; i <= localStorage.length; i++){
-        let currentEntry = localStorage.getItem(localStorage.key(i))
+        let currentEntry = localStorage.getItem(localStorage.key(i));
     if (currentEntry == someInput){
         // findCityData(queryURL)
         // cleanUpDynamicElements()
-        alert("Please input a non-duplicate entry")
-        return
+        alert("Please input a non-duplicate entry");
+        return;
     }
     }
-    putIntoLocalStorage(someInput)
-    cleanUpDynamicElements()
+    putIntoLocalStorage(someInput);
+    cleanUpDynamicElements();
     // pass the main API URL into the sequence
-    findCityData(someInput)
+    findCityData(someInput);
 }
+// cleans dynamic elements so new can be populated, sets a time to delay the elements to be update
 function cleanUpDynamicElements(){
     for(let i = 0; i <= localStorage.length; i++){
-        $('.city').remove()
-    }timer = setTimeout(getFromLocalStorage,100)
+        $('.city').remove();
+    }timer = setTimeout(getFromLocalStorage,100);
 }
 
-// pulls data from local storage
+// pulls data from local storage checks inputs
 function getFromLocalStorage(){ 
     if(localStorage.length <= 8){
     for(let i = 0; i < localStorage.length; i++){
-        let cityName = localStorage.getItem(localStorage.key(i))
-        let cityButton = $(`<button type="button" class="city">${cityName}</button>`)
-        cityButton.on('click', quickSearch)
-        let redBtn = $(`<button type="button" class="delete">x</button>`)
-        cityButton.append(redBtn)
-        $('.clear').before(cityButton)
+        let cityName = localStorage.getItem(localStorage.key(i));
+        let cityButton = $(`<button type="button" class="city">${cityName}</button>`);
+        cityButton.on('click', quickSearch);
+        let redBtn = $(`<button type="button" class="delete">x</button>`);
+        cityButton.append(redBtn);
+        $('.clear').before(cityButton);
     }
     }else{
         for(let i = 0; i < localStorage.length; i++){
-            let cityName = localStorage.getItem(localStorage.key(i))
-            let cityButton = $(`<button type="button" class="city">${cityName}</button>`)
-            let redBtn = $(`<button type="button" class="delete">x</button>`)
-            cityButton.append(redBtn)
-            $('.clear').before(cityButton)
+            let cityName = localStorage.getItem(localStorage.key(i));
+            let cityButton = $(`<button type="button" class="city">${cityName}</button>`);
+            let redBtn = $(`<button type="button" class="delete">x</button>`);
+            cityButton.append(redBtn);
+            $('.clear').before(cityButton);
     }
-    localStorage.removeItem(localStorage.key(8))
-    $('.city').eq(8).remove()
-    alert("You've reached max amount of entries")
+    localStorage.removeItem(localStorage.key(8));
+    $('.city').eq(8).remove();
+    alert("You've reached max amount of entries");
+    timer = setTimeout(getFromLocalStorage,100);
     }
 }
+// gets the text inside the gray button and cleans it so it can be reused to call up the API
 function quickSearch(event){
-    let grayButton = $(event.target)
+    let grayButton = $(event.target);
     if(grayButton){
-        let cleanButton = grayButton.closest('.city').text().trim()
-        cleanButton = cleanButton.slice(0, cleanButton.length-1)
-        $('#errMsg').remove()
-        findCityData(cleanButton)
+        let cleanButton = grayButton.closest('.city').text().trim();
+        cleanButton = cleanButton.slice(0, cleanButton.length-1);
+        $('#errMsg').remove();
+        findCityData(cleanButton);
     }
 }
+// clears out local storage and the list of buttons
 function clearAll(){
-    localStorage.clear()
-    $('input[type="search"]').focus()
-    $('input[type="search"]').val('')
-    cleanUpDynamicElements()
+    localStorage.clear();
+    $('input[type="search"]').focus();
+    $('input[type="search"]').val('');
+    cleanUpDynamicElements();
 }
-// function to remove items from local storage
+// function to remove a sinlge button when red box is clicked, also removes from local storage
 function removeSingleElement(event){
     let redBtn = $(event.target);
-    event.stopPropagation()
-    let buttonToRemove = redBtn.closest('.city')
-    let cityValueString = buttonToRemove.text()
-    cityValueString = cityValueString.slice(0, cityValueString.length-1)
+    event.stopPropagation();
+    let buttonToRemove = redBtn.closest('.city');
+    let cityValueString = buttonToRemove.text();
+    cityValueString = cityValueString.slice(0, cityValueString.length-1);
     for(let i = 0; i < localStorage.length; i++){
-        let itemToRemove = localStorage.getItem(localStorage.key(i))
+        let itemToRemove = localStorage.getItem(localStorage.key(i));
         if(cityValueString == itemToRemove){
-            localStorage.removeItem(localStorage.key(i))
+            localStorage.removeItem(localStorage.key(i));
         }
     }
-    buttonToRemove.remove()
-    $('input[type="search"]').val('')
+    buttonToRemove.remove();
+    $('input[type="search"]').val('');
 }
-
+// entry point of the script sequence
 function init(){
-    let srcBtn = $('.search')
+    let srcBtn = $('.search');
     srcBtn.on('click', function (event){
-        event.stopPropagation()
-        event.preventDefault()
-        let cityName = $('input[type="search"]').val().trim()
-        $('#errMsg').remove()
-        checkInput(cityName)
-        $('input[type="search"]').focus()
-        $('input[type="search"]').val('')
-        
-      
+        event.stopPropagation();
+        event.preventDefault();
+        let cityName = $('input[type="search"]').val().trim();
+        $('#errMsg').remove();
+        checkInput(cityName);
+        $('input[type="search"]').focus();
+        $('input[type="search"]').val('');
     })
 }
+// clears the main banner data
 function clearCityData(){
-    let dynamicElement1 = document.getElementById('time')
-    let dynamicElement2 = document.getElementById('status')
+    let dynamicElement1 = document.getElementById('time');
+    let dynamicElement2 = document.getElementById('status');
     if(dynamicElement1 || dynamicElement2){
-        dynamicElement1.remove()
-        dynamicElement2.remove()
+        dynamicElement1.remove();
+        dynamicElement2.remove();
     }else{return}
 }
+// clears the forecast section
 function clearFutureCast(){
     let futureCastElement = document.querySelectorAll('.days');
     if(futureCastElement){
         futureCastElement.forEach(futureCastElement=>{
-            futureCastElement.remove()
+            futureCastElement.remove();
         })
-    }return
+    }return;
 }
-
-
-
+// wait for page load, and setup execution 
 document.addEventListener('DOMContentLoaded', function(){
-    getFromLocalStorage()
-    init()
+    getFromLocalStorage();
+    init();
     $(document).on('click', '.delete', removeSingleElement);
-    $('.clear').click(clearAll)
+    $('.clear').click(clearAll);
 })
